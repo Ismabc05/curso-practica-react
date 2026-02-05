0c6f3bc30ebc466bcf411e80a180f5e01ee432fc
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-02-05 14:03:31 +0100

    React 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { render } from "react-dom"
+import ReactDOM from 'react-dom/client';
 import './estilos/index.css';
 import App from './App.js';
-
-
-
-const root = document.getElementById("root");
-render(<App/>, root);
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<App />);
